eb27fb9ca622c636379a97706907ee62510e13bb
Bri <bhilliker@gmail.com>
2021-10-14 09:14:42 -0500

    open link in new tab

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -9,7 +9,8 @@ const Modal = ({ onClose, currentPhoto }) => {
             <h3 className="modalTitle">{name} </h3>
                 <img src={require(`../../assets/images/${index}.png`).default} height="500" alt="current category" /> 
                 <button onClick={event => window.location.href=`${url}`}>Visit this App</button>
-                <button onClick={event => window.location.href=`${github}`}>Check Out the Github Repository</button>
+                <button onClick={() => window.open(`${github}`, `_blank`)}>Check Out the Github Repository</button>
+                {/* <button onClick={event => window.location.href=`${github}`}>Check Out the Github Repository</button> */}
                 {/* <Link to=" https://dry-wildwood-65158.herokuapp.com/">Deployed App</Link> */}
                 <p>{description}</p>
                 <button type="button" onClick={onClose}>
