41ecf1741c3c1e5315e8913efe5f085900a90876
Bri <bhilliker@gmail.com>
2021-10-14 08:59:24 -0500

    add links to  live app and repo

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const Modal = ({ onClose, currentPhoto }) => {
-    const { name, description, index  } = currentPhoto;
+    const { name, description, index, url, github  } = currentPhoto;
     
     return (
         <div className="modalBackdrop">
           <div className="modalContainer">
             <h3 className="modalTitle">{name} </h3>
                 <img src={require(`../../assets/images/${index}.png`).default} height="500" alt="current category" /> 
+                <button onClick={event => window.location.href=`${url}`}>Visit this App</button>
+                <button onClick={event => window.location.href=`${github}`}>Check Out the Github Repository</button>
+                {/* <Link to=" https://dry-wildwood-65158.herokuapp.com/">Deployed App</Link> */}
                 <p>{description}</p>
                 <button type="button" onClick={onClose}>
                     Close this modal
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,31 +8,48 @@ const Projects = ({category}) => {
     {
       name: 'weather',
       category: 'Portfolio',
+      url: `https://bcassellius.github.io/weather-dashboard/`,
+      github: `https://github.com/bcassellius/weather-dashboard.git`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
     {
       name: 'database',
       category: 'Portfolio',
+      github: `https://github.com/bcassellius/employee-tracker.git`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
     {
       name: 'books',
       category: 'Portfolio',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      url: 'https://dry-wildwood-65158.herokuapp.com/',
+      github: `https://github.com/bcassellius/book-tracker.git`,
+      description: `This is a group project that was created with Express, handlebars, and MySQL. 
+      
+      This is a live app that allows a user to login and keep track of books they've read and would like to read.
+
+      From the login page, a user can sign up and then later can login and the app will save the user's book data.
+      
+      A user's dashboard has a book form that can be filled out. Once the book is entered, it is added to the full list of books and is also sorted by user specification.`,
     },
     {
       name: 'utopia',
       category: 'Portfolio',
+      url: `https://bcassellius.github.io/Utopia/`,
+      github: `https://github.com/bcassellius/Utopia.git`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
     {
       name: 'budget',
       category: 'Portfolio',
+      url: `https://nameless-fortress-29034.herokuapp.com/`,
+      github: `https://github.com/bcassellius/budget-tracker.git`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
     {
       name: 'notes',
       category: 'Portfolio',
+      url: `https://secure-earth-84273.herokuapp.com/`,
+      github: `https://github.com/bcassellius/note-taker.git`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     }
   ]);
