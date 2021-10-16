9691de2a51dc124c7959f97713051150ff84f37f
Bri <bhilliker@gmail.com>
2021-10-16 18:56:39 -0500

    change image size in modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -9,7 +9,7 @@ const Modal = ({ onClose, currentPhoto }) => {
           <div className="modalContainer">
             <button className="btn modalButton close" type="button" onClick={onClose}>X</button>
             <h3 className="modalTitle">{name} </h3>
-                <img src={require(`../../assets/images/${index}.png`).default} class="center" height="500" alt="current category" /> 
+                <img src={require(`../../assets/images/${index}.png`).default} class="modalPic center" height="500" alt="current category" /> 
                 <p className="modalDescription">{description}</p>
                 <button className="btn modalButton center" type="button" onClick={() => window.open(`${url}`, `_blank`)}>Visit this App</button>
                 <button className="btn modalButton center" type="button" onClick={() => window.open(`${github}`, `_blank`)}>Check Out the Github Repository</button>
