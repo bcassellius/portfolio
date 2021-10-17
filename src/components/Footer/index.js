f3e7ba170eaa3882e3816bdfe352db75a999cd0e
Bri <bhilliker@gmail.com>
2021-10-16 21:29:57 -0500

    unstick footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,6 +6,10 @@ function Footer () {
 
     return(
        <div className="footer flex-row">
+            {/* <i class="fab fa-stack-overflow"></i>
+            <i class="fab fa-github"></i>
+            <i class="fab fa-linkedin"></i> */}
+
             <button type="button" className="btn btn-link"><a href="https://stackoverflow.com/users/15891840/bri" target="_blank">Stack Overflow</a></button>
             <button type="button" className="btn btn-link"><a href="https://www.linkedin.com/in/bri-cassellius-04286b211" target="_blank">LinkedIn</a></button>
             <button type="button" className="btn btn-link"><a href="https://github.com/bcassellius" target="_blank">Github</a></button>
