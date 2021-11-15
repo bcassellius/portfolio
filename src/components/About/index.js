cc8173382f7a7dba3d94e7add3b11d978d228a90
Bri <bhilliker@gmail.com>
2021-11-15 16:46:42 -0600

    add responsive columns for work examples

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -23,7 +23,71 @@ function About(props) {
                 <button type="button" className="btn btn-link"><a href="https://www.linkedin.com/in/bri-cassellius-04286b211" target="_blank">LinkedIn</a></button>
                 <button type="button" className="btn btn-link"><a href="https://github.com/bcassellius" target="_blank">Github</a></button>
                 <button type="button" className="btn btn-link"><a class="mailto" href="mailto:bhilliker@gmail.com"target="_blank">Email Me</a></button>
-            </div>        
+            </div>
+            
+            <div>
+                <div id="main">
+                    <div>
+                        <h3>Full Stack</h3>
+                        <ul className="skill">
+                            <li>
+                                <img src={require(`../../assets/images/0.png`).default} 
+                                    className="responsive-image"
+                                    alt="skills"
+                                />
+                            </li>
+                            <li>
+                                <img src={require(`../../assets/images/3.png`).default} 
+                                    className="responsive-image"
+                                    alt="skills"
+                                />
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>Front-End</h3>
+                        <ul className="skill">
+                            <li>
+                                <img src={require(`../../assets/images/4.png`).default} 
+                                    className="responsive-image"
+                                    alt="skills"
+                                />
+                            </li>
+                            <li>
+                                <img src={require(`../../assets/images/1.png`).default} 
+                                    className="responsive-image"
+                                    alt="skills"
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div>
+                        <h3>Back-End</h3>
+                        <ul className="skill">
+                            <li>
+                                <img src={require(`../../assets/images/2.png`).default} 
+                                    className="responsive-image"
+                                    alt="skills"
+                                />
+                            </li>
+                            <li>
+                                <img src={require(`../../assets/images/5.png`).default} 
+                                    className="responsive-image"
+                                    alt="skills"
+                                />
+                            </li>
+                            <li>
+                                <img src={require(`../../assets/images/6.png`).default} 
+                                    className="responsive-image"
+                                    alt="skills"
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
