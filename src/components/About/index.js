7d052ff77c504ff436abd63f23f8620101dca46a
Bri <bhilliker@gmail.com>
2021-11-26 19:24:47 -0600

    added detail accordion for each project

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import Projects from '../Projects';
 import avitar from "../../assets/images/avitar.png";
 import "./style.css";
 import { ExpandMore } from '@material-ui/icons';
-import { Accordion, AccordionSummary, AccordionDetails, AccordionActions, Divider, Button } from '@material-ui/core';
+import { Accordion, AccordionSummary, AccordionDetails, AccordionActions, Button } from '@material-ui/core';
 // accordion tutorial --> https://www.freakyjolly.com/react-material-ui-accordion-tabs-tutorial-with-example/
 
 
@@ -29,13 +29,9 @@ function About(props) {
                 <button type="button" className="btn btn-link"><a href="https://github.com/bcassellius" target="_blank">Github</a></button>
                 <button type="button" className="btn btn-link"><a class="mailto" href="mailto:bhilliker@gmail.com"target="_blank">Email Me</a></button>
             </div>
-            <div style={{ borderTop: "2px solid #3b3355ff ", marginLeft: 20, marginRight: 20 }}></div>
-            <div>
+            <div className='line'>
                 <div>
                     <h1 class="look me">Take a look at some of my recent projects...</h1>
-                    <h4 class="look me">
-                        For more details on these projects, check out my portfolio.
-                    </h4>
                 </div>
                 <div id="main">
                     <div>
@@ -45,7 +41,7 @@ function About(props) {
                                 <div >
                                     <img src={require(`../../assets/images/0.png`).default} 
                                         className="responsive-image"
-                                        alt="skills"
+                                        alt="project example"
                                     />
                                     <Accordion style={{background:`var(--background)`}}>
                                         <AccordionSummary expandIcon={<ExpandMore />}>
@@ -73,14 +69,34 @@ function About(props) {
                                     </Accordion>
                                 </div>                               
                             </li>
-                            <li>
-                                <a href='https://dry-wildwood-65158.herokuapp.com/' target='blank'>
-                                    <img src={require(`../../assets/images/3.png`).default} 
-                                        className="responsive-image"
-                                        alt="skills"
-                                    />
-                                    <p>Book Tracker</p>
-                                </a>
+                            <li className='line'>
+                                <img src={require(`../../assets/images/3.png`).default} 
+                                    className="responsive-image"
+                                    alt="project example"
+                                />
+                                <Accordion style={{background:`var(--background)`}}>
+                                    <AccordionSummary expandIcon={<ExpandMore />}>
+                                        Book Tracker
+                                    </AccordionSummary>
+                                    <AccordionDetails className='accord' >
+                                        This is a group project that was created with Express, handlebars, and MySQL. It is deployed using heroku. This is a live app that allows a user to login and keep track of books they've read and would like to read. From the login page, a user can sign up and then later can login and the app will save the user's book data. A user's dashboard has a book form that can be filled out. Once the book is entered, it is added to the full list of books and is also sorted by user specification.                                    </AccordionDetails>
+                                    <AccordionActions>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://github.com/bcassellius/book-tracker.git', '_blank')}>
+                                                    Github Repo
+                                        </Button>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://dry-wildwood-65158.herokuapp.com/', '_blank')}>
+                                                    Deployed App
+                                        </Button>
+                                    </AccordionActions>
+                                </Accordion>
                             </li>
                             <li>
                             <Projects stack={'full'} />
@@ -92,22 +108,64 @@ function About(props) {
                         <h3>Front-End</h3>
                         <ul className="skill">
                             <li>
-                                <a href='https://bcassellius.github.io/Utopia/' target='blank'>
-                                    <img src={require(`../../assets/images/4.png`).default} 
-                                        className="responsive-image"
-                                        alt="skills"
-                                    />
-                                    <p>Utopia Travel</p>
-                                </a>
+                                <img src={require(`../../assets/images/4.png`).default} 
+                                    className="responsive-image"
+                                    alt='project example'
+                                />
+                                <Accordion style={{background:`var(--background)`}}>
+                                    <AccordionSummary expandIcon={<ExpandMore />}>
+                                        Utopia Travel
+                                    </AccordionSummary>
+                                    <AccordionDetails className='accord' >
+                                        This is a group project that was created with JavaScript, JQuery, Open APIs, and styled with Materialize. It is deployed using github. This is a live app that has many features including a currency converter, weather search, and city search that displays restaurants, hotels, and weather.
+                                    </AccordionDetails>
+                                    <AccordionActions>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://github.com/bcassellius/Utopia.git', '_blank')}>
+                                                    Github Repo
+                                        </Button>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://bcassellius.github.io/Utopia/', '_blank')}>
+                                                    Deployed App
+                                        </Button>
+                                    </AccordionActions>
+                                </Accordion>
                             </li>
-                            <li>
-                                <a href='https://bcassellius.github.io/weather-dashboard/' target='blank'>
-                                    <img src={require(`../../assets/images/1.png`).default} 
-                                        className="responsive-image"
-                                        alt="skills"
-                                    />
-                                    <p>Weather Dashboard</p>
-                                </a>
+                            <li className='line'>
+                                <img src={require(`../../assets/images/1.png`).default} 
+                                    className="responsive-image"
+                                    alt='project example'
+                                />
+                                <Accordion style={{background:`var(--background)`}}>
+                                    <AccordionSummary expandIcon={<ExpandMore />}>
+                                        Weather Dashboard
+                                    </AccordionSummary>
+                                    <AccordionDetails className='accord' >
+                                        This is a weather app that was created with JavaScript and JQuery and styled using Bootstrap. It is deployed using github. This live app allows the user to search for destination weather. Once a city is entered and the search button is clicked, the current weather details and 5-day forcast details for that city will be displayed. Below the search box is a history of places you have searched.
+                                    </AccordionDetails>
+                                    <AccordionActions>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://github.com/bcassellius/weather-dashboard.git', '_blank')}>
+                                                    Github Repo
+                                        </Button>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://bcassellius.github.io/weather-dashboard/', '_blank')}>
+                                                    Deployed App
+                                        </Button>
+                                    </AccordionActions>
+                                </Accordion>
                             </li>
                             <li>
                             <Projects stack={'front'} />
@@ -119,31 +177,85 @@ function About(props) {
                         <h3>Back-End</h3>
                         <ul className="skill">
                             <li>
-                                <a href='https://github.com/bcassellius/employee-tracker.git' target='blank'>
-                                    <img src={require(`../../assets/images/2.png`).default} 
-                                        className="responsive-image"
-                                        alt="skills"
-                                    />
-                                    <p>Employee Tracker</p>
-                                </a>
+                                <img src={require(`../../assets/images/2.png`).default} 
+                                    className="responsive-image"
+                                    alt='project example'
+                                />
+                                <Accordion style={{background:`var(--background)`}}>
+                                    <AccordionSummary expandIcon={<ExpandMore />}>
+                                        Employee Tracker
+                                    </AccordionSummary>
+                                    <AccordionDetails className='accord' >
+                                        This is a group project responsive PWA that is used by librarians for internal library tasks. It is a MERN SPA that follows mobile-first practices and was created with React and styled using React Bootstrap and CSS. It is deployed using heroku. This app allows the user to search for books using the google books api as well as see the staff directory and events calendar.
+                                    </AccordionDetails>
+                                    <AccordionActions>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://github.com/bcassellius/employee-tracker.git', '_blank')}>
+                                                    Github Repo
+                                        </Button>
+                                    </AccordionActions>
+                                </Accordion>
                             </li>
-                            <li>
-                                <a href='https://nameless-fortress-29034.herokuapp.com/' target='blank'>
-                                    <img src={require(`../../assets/images/5.png`).default} 
-                                        className="responsive-image"
-                                        alt="skills"
-                                    />
-                                    <p>Budget Tracker</p>
-                                </a>
+                            <li className='line'>
+                                <img src={require(`../../assets/images/5.png`).default} 
+                                    className="responsive-image"
+                                    alt='project example'
+                                />
+                                <Accordion style={{background:`var(--background)`}}>
+                                    <AccordionSummary expandIcon={<ExpandMore />}>
+                                        Budget Tracker
+                                    </AccordionSummary>
+                                    <AccordionDetails className='accord' >
+                                        This Progressive Web App was created with node.js, mongoose, compression, and morgan. It is deployed using heroku. It tracks the budget by adding or subtracting funds. It can be used with or without internet access and will update the data when connected.                                    </AccordionDetails>
+                                    <AccordionActions>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://github.com/bcassellius/budget-tracker.git', '_blank')}>
+                                                    Github Repo
+                                        </Button>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://nameless-fortress-29034.herokuapp.com/', '_blank')}>
+                                                    Deployed App
+                                        </Button>
+                                    </AccordionActions>
+                                </Accordion>
                             </li>
-                            <li>
-                                <a href='https://secure-earth-84273.herokuapp.com/' target='blank'>
-                                    <img src={require(`../../assets/images/6.png`).default} 
-                                        className="responsive-image"
-                                        alt="skills"
-                                    />
-                                    <p>Note Taker</p>
-                                </a>
+                            <li className='line'>
+                                <img src={require(`../../assets/images/6.png`).default} 
+                                    className="responsive-image"
+                                    alt='project example'
+                                />
+                                <Accordion style={{background:`var(--background)`}}>
+                                    <AccordionSummary expandIcon={<ExpandMore />}>
+                                        Note Taker
+                                    </AccordionSummary>
+                                    <AccordionDetails className='accord' >
+                                        This app was created with node.js and express. It is deployed using heroku. The app allows the user to write and access notes they created.                                    </AccordionDetails>
+                                    <AccordionActions>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://github.com/bcassellius/note-taker.git', '_blank')}>
+                                                    Github Repo
+                                        </Button>
+                                        <Button 
+                                            style={{ border: `2px solid #3b3355ff`}} 
+                                            size="small" 
+                                            onClick={() => 
+                                                window.open('https://secure-earth-84273.herokuapp.com/', '_blank')}>
+                                                    Deployed App
+                                        </Button>
+                                    </AccordionActions>
+                                </Accordion>
                             </li>
                             <li>
                             <Projects stack={'back'} />
