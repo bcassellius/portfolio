c32a0aedc1a3ef1af1249212cda453db0ed962b1
Bri <bhilliker@gmail.com>
2021-12-01 14:19:43 -0600

    updated downloadable resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,7 +6,7 @@ function Resume() {
     return(
         <div>
             <div>
-                <button type="button" className="btn download btn-link"><a href="https://github.com/bcassellius/portfolio/raw/main/public/resume2021.pdf" download="bri" >Download Resume</a></button>
+                <button type="button" className="btn download btn-link"><a href="https://github.com/bcassellius/portfolio/raw/main/public/cassellius21.pdf" download="bri" >Download Resume</a></button>
             </div>
             <img src={require(`../../assets/images/words9.png`).default} 
                 className="responsive-image"
