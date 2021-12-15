f04613c95af47e4ffbd834e97b73290d8a23e882
Bri <bhilliker@gmail.com>
2021-12-14 18:51:20 -0600

    changed button to span so you can highlight

--- src/components/Email/index.js
+++ src/components/Email/index.js
@@ -8,19 +8,19 @@ function Email() {
     <section>
         <div className="flex-row">
             <button type="button" className="btn btn-link"><a href="https://stackoverflow.com/users/15891840/bri" target="_blank"><FaStackOverflow /></a></button>
-            <button disabled className="detail">https://stackoverflow.com/users/15891840/bri</button>
+            <span className="detail">https://stackoverflow.com/users/15891840/bri</span>
         </div>
         <div className="flex-row">
             <button type="button" className="btn btn-link"><a href="https://www.linkedin.com/in/bri-cassellius-04286b211" target="_blank"><FaLinkedin /></a></button>
-            <button disabled className="detail">https://www.linkedin.com/in/bri-cassellius-04286b211</button>
+            <span className="detail">https://www.linkedin.com/in/bri-cassellius-04286b211</span>
         </div>
         <div className="flex-row">
             <button type="button" className="btn btn-link"><a href="https://github.com/bcassellius" target="_blank"><FaGithub /></a></button>
-            <button disabled className="detail">https://github.com/bcassellius</button>
+            <span className="detail">https://github.com/bcassellius</span>
         </div>
         <div className="flex-row">
             <button type="button" className="btn btn-link"><a class="mailto" href="mailto:bhilliker@gmail.com"target="_blank"><FaRegEnvelope /></a></button>
-            <button disabled className="detail">bhilliker@gmail.com</button>
+            <span className="detail">bhilliker@gmail.com</span>
         </div>
     </section>
   );
